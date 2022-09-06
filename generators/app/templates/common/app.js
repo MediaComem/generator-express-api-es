7cb09600cdb9a2e8c3e68a6a972a5ffdba9a1314
Simon Pinkas <212782+simonpinkas@users.noreply.github.com>
2022-09-06 09:04:59 +0200

    Remove view engine and public directory from express setup

--- generators/app/templates/common/app.js
+++ generators/app/templates/common/app.js
@@ -12,14 +12,10 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
